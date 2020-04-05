82b383d307c6ad7adc5f938456d91fb49336ca72
fenomeno86 <ducduy.cn@gmail.com>
2020-04-05 23:15:07 +0700

    add folder chapter_1

---
+++ chapter_1/src/package11/KDW.java
@@ -0,0 +1,18 @@
+package package11;
+
+public class KDW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("**        ***    **********    **              *             **");
+		System.out.println("**      ***      **        **   **            ***           **");
+		System.out.println("**    ***        **         **   **          ** **         **");
+		System.out.println("**  ***          **          **   **        **   **       **");
+		System.out.println("*****            **          **    **      **     **     **");
+		System.out.println("**  ***          **         **      **    **       **   **");
+		System.out.println("**    ***        **        **        **  **         ** **");
+		System.out.println("**      ***      **       **          ***            ***");
+		System.out.println("**        ***    **********            *              *");
+	}
+
+}
